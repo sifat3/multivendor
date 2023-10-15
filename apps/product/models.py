83f3f727884de765b990787a1285d78b3b8a95e3
Ashraful Haque Sifat <ahsifat321@gmail.com>
2023-10-16 01:14:35 +0600

    2

--- apps/product/models.py
+++ apps/product/models.py
@@ -47,16 +47,17 @@ class Product(models.Model):
                 return 'https://via.placeholder.com/240x180.jpg'
     
     def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
-        img.convert('RGB')
-        img.thumbnail(size)
+        # img = Image.open(image)
+        # img.convert('RGB')
+        # img.thumbnail(size)
 
-        thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
+        # thumb_io = BytesIO()
+        # img.save(thumb_io, 'JPEG', quality=85)
 
-        thumbnail = File(thumb_io, name=image.name)
+        # thumbnail = File(thumb_io, name=image.name)
 
-        return thumbnail
+        # return thumbnail
+        return image
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
